e1d381ae310ce9a3a5a7d29c55d9ee6b813c4f26
kingjr <jeanremi.king@gmail.com>
2024-12-16 13:04:34 +0100

    config.build

--- docs/infra/example_lightning.py
+++ docs/infra/example_lightning.py
@@ -10,27 +10,12 @@ from torchvision import datasets, transforms
 from torchvision.models import resnet18
 
 
-class ModelConfig(pydantic.BaseModel):
-    pretrained: bool = True
-    learning_rate: float = 0.001
-    model_config = pydantic.ConfigDict(extra="forbid")
-
-
-class DataConfig(pydantic.BaseModel):
-    batch_size: int = 64
-    model_config = pydantic.ConfigDict(extra="forbid")
-
-
-class TrainerConfig(pydantic.BaseModel):
-    max_epochs: tp.Optional[int] = None
-    model_config = pydantic.ConfigDict(extra="forbid")
-
-
 class Model(pl.LightningModule):
-    def __init__(self, config: ModelConfig = ModelConfig()):
+    def __init__(self, pretrained: bool, learning_rate: float = 0.001):
         super(Model, self).__init__()
-        self.config = config
-        self.model = resnet18(pretrained=config.pretrained)
+        self.pretrained = pretrained
+        self.learning_rate = learning_rate
+        self.model = resnet18(pretrained=pretrained)
         self.model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
         self.model.fc = torch.nn.Linear(self.model.fc.in_features, 10)
         self.loss_fn = torch.nn.CrossEntropyLoss()
@@ -55,18 +40,18 @@ class Model(pl.LightningModule):
         return loss
 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.model.parameters(), lr=self.config.learning_rate)
+        return torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
 
 
 class Data(pl.LightningDataModule):
-    def __init__(self, config: DataConfig):
+    def __init__(self, batch_size: int):
         super().__init__()
-        self.config = config
+        self.batch_size = batch_size
 
     def _dataloader(self, train: bool):
         transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
         dset = datasets.MNIST('', train=train, download=True, transform=transform)
-        return torch.utils.data.DataLoader(dset, batch_size=self.config.batch_size)
+        return torch.utils.data.DataLoader(dset, batch_size=self.batch_size)
 
     def train_dataloader(self):
         return self._dataloader(train=True)
@@ -75,28 +60,48 @@ class Data(pl.LightningDataModule):
         return self._dataloader(train=False)
     
 
+
+class ModelConfig(pydantic.BaseModel):
+    pretrained: bool = True
+    learning_rate: float = 0.001
+    
+    model_config = pydantic.ConfigDict(extra="forbid")
+
+    def build(self) -> Model:
+        return Model(**self.dict())
+
+
+class DataConfig(pydantic.BaseModel):
+    batch_size: int = 64
+    
+    model_config = pydantic.ConfigDict(extra="forbid")
+
+    def build(self) -> Data:
+        return Data(**self.dict())
+
+
+class TrainerConfig(pydantic.BaseModel):
+    max_epochs: tp.Optional[int] = None
+    
+    model_config = pydantic.ConfigDict(extra="forbid")
+
+    def build(self, checkpoint_path: str | None = None) -> Trainer:
+        if checkpoint_path:
+            callbacks = [ModelCheckpoint(
+                dirpath=checkpoint_path,
+                save_top_k=1,
+                monitor="val_loss",
+                mode="min")
+            ]
+        else:
+            callbacks = None
+        return Trainer(**self.dict(), callbacks=callbacks)
+
 class Experiment(pydantic.BaseModel):
     model: ModelConfig = ModelConfig()
     data: DataConfig = DataConfig()
     trainer: TrainerConfig = TrainerConfig()
     infra: exca.TaskInfra = exca.TaskInfra(folder='.cache/')
-
-    def build(self):
-        data = Data(self.data)
-        model = Model(self.model)
-        
-        # Add checkpoint callback based on infra folder
-        callbacks = None
-        if self.infra.folder:
-            callbacks = [ModelCheckpoint(
-                dirpath=self.infra.uid_folder() / 'checkpoint',
-                save_top_k=1,
-                monitor="val_loss",
-                mode="min")
-            ]
-
-        trainer = Trainer(**self.trainer.dict(), callbacks=callbacks)
-        return data, model, trainer
 
     @property
     def checkpoint_path(self):
@@ -111,22 +116,27 @@ class Experiment(pydantic.BaseModel):
     @infra.apply
     def fit(self):
         # Configure
-        data_loaders, model, trainer = self.build()
-        
+        data = self.data.build()
+        model = self.model.build()
+        trainer = self.trainer.build(self.infra.folder)
+
         # Fit model
-        trainer.fit(model, data_loaders, ckpt_path=self.checkpoint_path)
+        trainer.fit(model, data, ckpt_path=self.checkpoint_path)
         
         # Return model if not saved 
         if self.checkpoint_path is None:
             return model
 
     def validate(self):
-        data_loaders, model, trainer = self.build()
+        data = self.data.build()
+        model = self.model.build()
+        trainer = self.trainer.build(self.infra.folder)
+
         trained_model = self.fit()
         if trained_model is None:
             trained_model = model.__class__.load_from_checkpoint(self.checkpoint_path)
 
-        return trainer.validate(trained_model, dataloaders=data_loaders.val_dataloader())
+        return trainer.validate(trained_model, dataloaders=data.val_dataloader())
 
 
 if __name__ == '__main__':
