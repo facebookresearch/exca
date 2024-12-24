53aa337176e92c8791ba4fb827eab0e30f04c997
Jérémy Rapin <jrapin@meta.com>
2024-12-24 10:45:38 +0100

    Update example_sklearn.py

--- docs/infra/example_sklearn.py
+++ docs/infra/example_sklearn.py
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
 """
 A minimalist example with sklearn to show how to develop and explore a model with exca.
 """
