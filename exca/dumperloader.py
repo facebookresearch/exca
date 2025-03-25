25a98984d40a17cdfa07f346f1193f9c9b65d920
Jeremy Rapin <jrapin@meta.com>
2025-03-25 14:07:32 +0100

    raise

--- exca/dumperloader.py
+++ exca/dumperloader.py
@@ -184,6 +184,8 @@ class MemmapArrayFile(DumperLoader[np.ndarray]):
             raise RuntimeError("Need a write_mode context")
         if not isinstance(value, np.ndarray):
             raise TypeError(f"Expected numpy array but got {value} ({type(value)})")
+        if not value.size:
+            raise ValueError(f"Cannot dump data with no size: shape={value.shape}")
         offset = self._f.tell()
         self._f.write(np.ascontiguousarray(value).data)
         return {
