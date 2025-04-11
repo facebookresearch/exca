3f9dd885acbdf76b9af1bae7fdb3d175fb7238e8
Jeremy Rapin <jrapin@meta.com>
2025-04-11 13:34:30 +0200

    fix

--- exca/map.py
+++ exca/map.py
@@ -37,8 +37,8 @@ Mode = tp.Literal["cached", "force", "read-only"]
 def _set_tqdm(items: X, total: int | None = None) -> X:
     # (incorrect typing but nevermind)
     if total is None:
-        total == len(items)  # type: ignore
-    if total <= 1:  # type: ignore
+        total = len(items)  # type: ignore
+    if total <= 1:
         return items
     try:
         import tqdm
