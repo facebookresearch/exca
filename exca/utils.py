476bc8a2ddce1d844e31a5756a40a92e0f5bbcaf
Jérémy Rapin <jrapin@meta.com>
2025-06-23 18:27:08 +0200

    Improve error message for failed schema extraction (#90)

--- exca/utils.py
+++ exca/utils.py
@@ -280,7 +280,15 @@ def _set_discriminated_status(
         discriminator: str = DiscrimStatus.NONE
         if schema is None and len(_pydantic_hints(field.annotation)) > 1:
             # compute schema only if finding a possible pydantic union, as it is slow
-            schema = obj.model_json_schema()
+            try:
+                schema = obj.model_json_schema()
+            except Exception:
+                from .confdict import ConfDict
+
+                msg = "Failed to extract schema for type %s:\n%s\nFull yaml:\n%s"
+                cfg = ConfDict.from_model(obj, uid=False, exclude_defaults=False)
+                logger.warning(msg, obj.__class__.__name__, repr(obj), cfg.to_yaml())
+                raise
         if schema is not None:
             discriminator = _get_discriminator(schema, name)
         value = getattr(obj, name, _default)  # use _default for backward compat
