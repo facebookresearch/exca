1721c9b01c9f98bbc6477e6699e219dd42d6129e
Jeremy Rapin <jrapin@meta.com>
2025-12-10 16:14:22 +0100

    note

--- exca/chain/__init__.py
+++ exca/chain/__init__.py
@@ -3,6 +3,9 @@
 #
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
+"""Experimental API of chainable steps with 1 input and 1 output
+Note: this API is unstable, use at your own risk
+"""
 
 from . import backends as backends
 from .steps import Chain as Chain
