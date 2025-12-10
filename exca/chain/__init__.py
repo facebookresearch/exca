205c0667be586961705b1fc9901fbafb10f1ad0d
Jeremy Rapin <jrapin@meta.com>
2025-12-10 15:27:47 +0100

    Add experimental Chain API

---
+++ exca/chain/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+from . import backends as backends
+from .steps import Chain as Chain
+from .steps import Step as Step
