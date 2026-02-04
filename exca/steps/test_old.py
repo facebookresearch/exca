de9aa62925b75eced299c8c19d62346fee1bd3d6
Jeremy Rapin <jrapin@meta.com>
2026-02-04 20:56:32 +0100

    header

--- exca/steps/test_old.py
+++ exca/steps/test_old.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 """Old test versions kept temporarily during consolidation.
 
 These tests were replaced by consolidated versions. If these fail while
