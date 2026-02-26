405a5308c47e5d8245b31774ea2f83230d4ff350
Corentin Bel <corentin.bel@protonmail.com>
2026-02-26 17:21:46 +0100

    cleaner, prettier, better, faster

--- docs/conf.py
+++ docs/conf.py
@@ -1,31 +1,30 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sys
 from pathlib import Path
+
+# Make sure exca is importable for autodoc
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
 project = "Exca"
 copyright = "Meta Platforms, Inc"
 author = "FAIR"
-release = "0.1"
-
+release = "0.5.15"
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     "myst_parser",
     "sphinx.ext.autodoc",
     "sphinx.ext.linkcode",
-    # "sphinx.ext.autosectionlabel",
-    # "sphinx.ext.githubpages",
-    # "sphinx.ext.coverage",
-    # "sphinx.ext.napoleon",
-    # "sphinx.ext.autosummary",
-    # "recommonmark",
+    "sphinx.ext.napoleon",
+    "sphinx_design",
+    "sphinx_copybutton",
+]
+
+myst_enable_extensions = [
+    "colon_fence",
+    "fieldlist",
+    "substitution",
 ]
 
 templates_path = ["_templates"]
@@ -42,12 +41,59 @@ exclude_patterns = [
 # `path/to/file:heading` instead of just `heading`
 autosectionlabel_prefix_document = True
 
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
-html_static_path = []  # ["_static"]
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
+html_favicon = "_static/exca.png"
+
+html_theme_options = {
+    "logo": {
+        "image_light": "_static/exca.png",
+        "image_dark": "_static/exca.png",
+        "text": "Exca",
+    },
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/facebookresearch/exca",
+            "icon": "fa-brands fa-github",
+        },
+        {
+            "name": "PyPI",
+            "url": "https://pypi.org/project/exca/",
+            "icon": "fa-brands fa-python",
+        },
+    ],
+    "header_links_before_dropdown": 6,
+    "navbar_align": "left",
+    "show_toc_level": 2,
+    "navigation_with_keys": False,
+    "show_prev_next": True,
+    "footer_start": ["copyright"],
+    "footer_end": ["last-updated"],
+    "secondary_sidebar_items": ["page-toc", "edit-this-page"],
+    "pygments_light_style": "friendly",
+    "pygments_dark_style": "monokai",
+    "announcement": None,
+}
+
+html_context = {
+    "github_user": "facebookresearch",
+    "github_repo": "exca",
+    "github_version": "main",
+    "doc_path": "docs",
+}
+
+html_sidebars = {
+    "index": [],
+    "**": ["sidebar-nav-bs"],
+}
+
+html_show_sourcelink = False
+
+# -- Linkcode ----------------------------------------------------------------
 
 
 def linkcode_resolve(domain, info):
